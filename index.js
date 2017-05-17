bf50d4193b593588f9a2c66def961a7f55c4db74
Joseph Palacio <jpalacio@nectarom.com>
2017-05-17 16:26:26 -0500

    Refactor index for minor changes.

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const argv = require('minimist')(process.argv.slice(2), {
        m: 'message',
        n: 'message',
        x: 'dry-run',
+       b: 'branch',
        h: 'help',
-       n: 'rename',
-       name: 'rename',
+       r: 'rename',
+       d: 'delete'
     }
 });
+const {log, error} = console;
 
 
-function fmtBranchName(message) {
+function frmtBranchName(message) {
   let counter = 0
   function sort(ele){
     switch(ele) {
@@ -24,24 +26,63 @@ function fmtBranchName(message) {
   return message.replace(/\s/, '/').replace(/[\s.]/g, sort).toLowerCase();
 }
 
-function fmtMessage(message) {
+function frmtMessage(message) {
   return message.replace(/\./g, '').slice(0,1).toUpperCase() + message.slice(1) + '.';
+}
+
+if (argv.help) log(
+`
+Options:
+  -h, --help              usage information
+  -rn, -rm ,--rename + -m how to rename
+  -b, --branch            add branch name
+`
+);
+
+if (argv.delete) {
+  !argv.x && exec(`git push origin :${argv.delete} && git branch -D ${argv.delete} && echo bob && echo test`,
+    (er, so, se) => {
+      if (er) {
+        console.error(`exec error: ${er}`);
+        return;
+      }
+      log(`stdout: ${so}`)
+      log(`stderr: ${se}`);
+    }
+  );
 }
 
 if (argv.help) console.log('--rename + -m or -rn or -rm');
 
 if(argv.rename) {
   if (argv.message) {
-    const message = fmtMessage(argv.message)
-    const branchName = fmtBranchName(message)
-    !argv.x && exec(`git push origin :$(git_current_branch) && git branch -m ${branchName} && git commit --amend -m'${message}' && git push --set-upstream origin ${branchName}`,
-      (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
+     exec(`git symbolic-ref --short HEAD`, (er, current_branch, se) => {
+      log('current_branch', current_branch);
+      if (er) {
+        console.error(`exec error: ${er}`);
         return;
       }
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
+      const message = frmtMessage(argv.message);
+      const branchName = argv.b ? arg.b : frmtBranchName(message);
+      !argv.x && exec(`git push origin :${current_branch}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        log(`stdout: ${stdout}`);
+        log(`stderr: ${stderr}`);
+
+        exec(`git branch -m ${branchName}  && git commit --amend -m\'${message}\' && git push --set-upstream origin ${branchName}`, (err, stdo, stde) => {
+          if (err) {
+            console.error(`exec error: ${err}`);
+            return;
+          }
+          log(`stdout: ${stdo}`);
+          log(`stderr: ${stde}`);
+        })
+      });
+      log(`stderr: ${se}`);
+    })
   }
 }
