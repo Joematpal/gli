ad7dcc67fbc4cf9fc6d49fa81220714285aed115
Joseph Palacio <jpalacio@nectarom.com>
2017-05-17 16:26:26 -0500

    Add format message for periods

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const { exec } = require('child_process');
 const argv = require('minimist')(process.argv.slice(2), {
     alias: {
        m: 'message',
+       n: 'message',
        x: 'dry-run',
        h: 'help',
        n: 'rename',
        name: 'rename',
     }
 });
+
 
 function fmtBranchName(message) {
   let counter = 0
@@ -22,17 +24,17 @@ function fmtBranchName(message) {
   return message.replace(/\s/, '/').replace(/[\s.]/g, sort).toLowerCase();
 }
 
-function fmtMessage(message){
-  if(message.slice(-1) !== '.') message += '.';
-  const newmessage = message.slice(0,1).toUpperCase() + message.slice(1);
-  return newmessage;
+function fmtMessage(message) {
+  return message.replace(/\./g, '').slice(0,1).toUpperCase() + message.slice(1) + '.';
 }
+
+if (argv.help) console.log('--rename + -m or -rn or -rm');
 
 if(argv.rename) {
   if (argv.message) {
     const message = fmtMessage(argv.message)
     const branchName = fmtBranchName(message)
-    !argv.x && exec(`git push origin :$(git_current_branch) && git branch -m ${branchName} && git commit --amend -m'${message}' && git push --set-upstream origin $(git_current_branch)`,
+    !argv.x && exec(`git push origin :$(git_current_branch) && git branch -m ${branchName} && git commit --amend -m'${message}' && git push --set-upstream origin ${branchName}`,
       (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
